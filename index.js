23db7ad521e1b373a3f0873b95bb781cbcbf73ee
Alex <alex.stormwood@bigfootds.com>
2026-01-19 21:04:06 +1100

    feat: initial example code

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello, world! From the JS file!");
+
+// let is locally-scoped and mutable
+let favouriteFruit = "mango";
+
+// const is immutable 
+const favouriteFruit2 = "mango";
+
+/*
+
+some big long warning about the dangers of var
+goes here
+pretend it's scary, oooooooh
+
+*/
+var favouriteFruit3 = "mango";
+
+
+/**
+ * Multi-line comment with cool structure.
+ * JSDoc comment style!
+ * @example console.log(someOtherVariableExample);
+ */
+let someOtherVariableExample = "bananas";
+
+
+console.log(someOtherVariableExample)
